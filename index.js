8a7f7b7ef53925cc777dae1575e7a042dbbc6416
Konrad Goldammer <konradgoldammer@gmail.com>
2022-01-29 21:06:12 +0100

    Add plog/flog types

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import * as plogTypes from "./plogTypes.js";
+import * as flogTypes from "./flogTypes.js";
+
+export const plogTypes;
+export const flogTypes;
+
 export * from "./models/Abonnement.js";
 export * from "./models/Account.js";
 export * from "./models/Category.js";
