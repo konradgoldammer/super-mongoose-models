792a11a1633f17ab34210468eea4b35552b69caa
Konrad Goldammer <konradgoldammer@gmail.com>
2022-02-17 16:34:41 +0100

    Increase expiration time of flogs and plogs

--- models/Flog.js
+++ models/Flog.js
@@ -6,6 +6,6 @@ export const Flog = mongoose.model(
     type: { type: String, required: true },
     content: { type: String },
     account: { type: mongoose.Schema.Types.ObjectId },
-    createdAt: { type: Date, expires: 3600, default: Date.now }, // doc expires after 1 hour
+    createdAt: { type: Date, expires: 345600, default: Date.now }, // doc expires after 4 days
   })
 );
